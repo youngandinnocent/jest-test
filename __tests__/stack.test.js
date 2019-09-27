1ae4fab80cc935a2b22dbbeb51d10bafcf63f6a8
Vanek <ivan.bermann@gmail.com>
2019-09-27 23:54:25 +0300

    fixed linter errors

--- __tests__/stack.test.js
+++ __tests__/stack.test.js
@@ -3,21 +3,20 @@ import makeStack from '../src/stack';
 const stack = makeStack;
 
 test("stack's main flow", () => {
-    stack.push('one');
-    stack.push('two');
-    expect(stack.pop()).toEqual('two');
-    expect(stack.pop()).toEqual('one');
+  stack.push('one');
+  stack.push('two');
+  expect(stack.pop()).toEqual('two');
+  expect(stack.pop()).toEqual('one');
 });
 
 test('isEmpty', () => {
-    expect(stack.isEmpty()).toBe(true);
-    stack.push('two');
-    expect(stack.isEmpty()).toBe(false);
-    stack.pop();
-    expect(stack.isEmpty()).toBe(true);
+  expect(stack.isEmpty()).toBe(true);
+  stack.push('two');
+  expect(stack.isEmpty()).toBe(false);
+  stack.pop();
+  expect(stack.isEmpty()).toBe(true);
 });
 
 test('pop in empty stack', () => {
-    expect(() => stack.pop()).toThrow();
+  expect(() => stack.pop()).toThrow();
 });
-
--- src/stack.js
+++ src/stack.js
@@ -1,29 +1,31 @@
-export default new Stack();
-
 function Stack() {
-    this.size = 0;
-    this.storage = {};
+  this.size = 0;
+  this.storage = {};
 }
 
-Stack.prototype.push = function(data) {
-    let size = ++this.size;
-    this.storage[size] = data;
-}
+Stack.prototype.push = function push(data) {
+  this.size += 1;
+  const { size } = this;
+  this.storage[size] = data;
+};
 
-Stack.prototype.pop = function() {
-    let size = this.size;
-    let deletedData;
-    
-    if (size) {
-        deletedData = this.storage[size];
-        
-        delete this.storage[size];
-        this.size--;
-        return deletedData;
-    }
-    throw new Error('Stack is empty!');
-}
+Stack.prototype.pop = function pop() {
+  const { size } = this;
+  let deletedData;
+  if (size) {
+    deletedData = this.storage[size];
+    delete this.storage[size];
+    this.size -= 1;
+    return deletedData;
+  }
+  throw new Error('Stack is empty!');
+};
 
-Stack.prototype.isEmpty = function() {
-    return this.size === 0 ? true : false;
-}
+Stack.prototype.isEmpty = function isEmpty() {
+  if (this.size === 0) {
+    return true;
+  }
+  return false;
+};
+
+export default new Stack();
